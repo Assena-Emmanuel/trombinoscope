cf4a2c136d565148d118701836d8a50261b6901d
Nosttres <assenaemmanuel3@gmail.com>
2025-01-05 06:37:53 +0100

    Enregistrer une personne

--- milaTrombi/App/models.py
+++ milaTrombi/App/models.py
@@ -1,3 +1,50 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class Personne(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = None
+    last_name = None
+    first_name = None
+
+    nom = models.CharField(max_length=30)
+    prenom = models.CharField(max_length=30)
+    telephone = models.CharField(max_length=30)
+    adresse = models.CharField(max_length=200)
+    photo = models.ImageField(upload_to="photos/")
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['nom', 'prenom', 'telephone', 'adresse', 'photo']
+
+class Cv(models.Model):
+    poste = models.CharField(max_length=225)
+    description = models.TextField()
+    persone = models.ForeignKey(Personne, on_delete=models.CASCADE)
+
+class Formation(models.Model):
+    cv = models.ForeignKey(Cv, on_delete=models.CASCADE)
+    etablissement = models.CharField(max_length=255)
+    diplome = models.CharField(max_length=255)
+    periode = models.CharField(max_length=4)
+
+class Loisir(models.Model):
+    cv = models.ForeignKey(Cv, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+class ExperienceProfessionnelle(models.Model):
+    cv = models.ForeignKey(Cv, on_delete=models.CASCADE)
+    job_titre = models.CharField(max_length=255)
+    entreprise = models.CharField(max_length=255)
+    periode = models.CharField(max_length=50)
+    description = models.TextField()
+
+class Langue(models.Model):
+    cv = models.ForeignKey(Cv, on_delete=models.CASCADE)
+    language = models.CharField(max_length=100)
+    niveau = models.CharField(max_length=50)
+
+class Competence(models.Model):
+    cv = models.ForeignKey(Cv, on_delete=models.CASCADE)
+    competence = models.CharField(max_length=255)
+    niveau = models.CharField(max_length=50)
--- milaTrombi/App/views.py
+++ milaTrombi/App/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+def showCv(request):
+    return render(request, 'formatCv/defaut.html')
+
+# enregistrer une nouvelle personne
+def ajouterPersonne(request):
+    if request.method == "POST":
+        pass
+
+    else:
+        return render(request, 'cv/ajouterPersonne.html')
